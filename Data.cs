537b60c85789fdc115d065c5f615b17af6d1a52e
Batanai <31771294+Batanai@users.noreply.github.com>
2020-11-06 08:15:18 +0200

    finalCommit

---
+++ Data.cs
@@ -0,0 +1,10 @@
+namespace Excercise2
+{
+    public class Data
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead("./Data.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
+           
+
+            List<Data> data = new List<Data>();
+
+            for (int i = 0; i < csvTable.Rows.Count; i++)  
+                {  
+                    data.Add(new Data 
+                    { 
+                        FirstName= csvTable.Rows[i][0].ToString(), 
+                        LastName= csvTable.Rows[i][1].ToString(), 
+                        Address = csvTable.Rows[i][2].ToString(), 
+                        PhoneNumber = csvTable.Rows[i][3].ToString() 
+                    });
+                    Console.WriteLine();
+                    
+                }
+            
+        }
+    }
+}
